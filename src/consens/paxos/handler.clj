61d54d96f2b54a490a919bb5d4ffcd92a998211c
Robin Ricard <ricard.robin@gmail.com>
2014-12-30 21:28:20 +0100

    Fix indent in paxos handler

--- src/consens/paxos/handler.clj
+++ src/consens/paxos/handler.clj
@@ -10,14 +10,14 @@
       (res/content-type "text/plain")))
 
 (defn app
- "Initialize and return an handler closure.
- Initializes by joining the cluster or by starting empty."
- [cluster join?]
- (let [storage (atom {})
-       snbuf (atom {})]
-   (do
-     (if join?
-       (do
-         (swap! storage #(%2) (remote/get-storage (first cluster)))
-         (swap! snbuf #(%2) (remote/get-snbuf (first cluster)))))
-     (partial handler cluster storage snbuf))))
+  "Initialize and return an handler closure.
+  Initializes by joining the cluster or by starting empty."
+  [cluster join?]
+  (let [storage (atom {})
+        snbuf (atom {})]
+    (do
+      (if join?
+        (do
+          (swap! storage #(%2) (remote/get-storage (first cluster)))
+          (swap! snbuf #(%2) (remote/get-snbuf (first cluster)))))
+      (partial handler cluster storage snbuf))))
